971f7c2bb0b108f360c768fea6fb49051b64aebf
Eva Nunez <evahlivkova@MacBook-Pro-2.local>
2025-05-17 14:48:18 +0200

    Added option to send reservation details and redirecting to reservation detail page upon reservation confirmation

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,18 +2,49 @@ import { JourneyPicker } from '../../components/JourneyPicker';
 import { JourneyDetail } from '../../components/JourneyDetail/journeydetail';
 import { SelectedSeat } from '../../components/SelectedSeat/SelectedSeat';
 import { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { use } from 'react';
 
 export const HomePage = () => {
   const [journey, setJourney] = useState(null);
   const handleJourneyChange = (journeyData) => {
     setJourney(journeyData);
-  }
+  };
+
+  const navigate = useNavigate();
+
+
+  const handleBuy = async () => {
+    const response = await fetch(
+      'https://apps.kodim.cz/daweb/leviexpress/api/reservation',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          action: 'create',
+          seat: journey.autoSeat,
+          journeyId: journey.journeyId,
+        }),
+      },
+    );
+    const responseData = await response.json();
+    const reservationId = responseData.results.reservationId;
+    navigate(`/reservation/${reservationId}`);
+  };
   return (
     <main>
-      <JourneyPicker onJourneyChange={handleJourneyChange}/>
-      <p>{journey === null ?'' : <JourneyDetail journey={journey}/>}</p>
-      <p>{journey === null ?'' : <SelectedSeat number={journey.autoSeat}/>}</p>
-      
+      <JourneyPicker onJourneyChange={handleJourneyChange} />
+      <p>{journey === null ? '' : <JourneyDetail journey={journey} />}</p>
+      <p>
+        {journey === null ? '' : <SelectedSeat number={journey.autoSeat} />}
+      </p>
+      <div class="controls container">
+        <button onClick={handleBuy} class="btn btn--big" type="button">
+          Rezervovat
+        </button>
+      </div>
     </main>
   );
 };
